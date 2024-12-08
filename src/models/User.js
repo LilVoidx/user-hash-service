66b5dc176ab6de1c01163d0226ee5aa6fb4d9cd2
Void <youssefmagdyanwar@yandex.ru>
2024-12-08 17:29:38 +0300

    Changed User Schema to auto gen id

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+    default: () => new mongoose.Types.ObjectId().toString(),
+  },
   name: { type: String, required: true },
   about: { type: String, required: true },
 });
